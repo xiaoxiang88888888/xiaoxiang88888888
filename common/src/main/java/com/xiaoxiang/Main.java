1d650f56e2289073b933087634af5d3b9da3c777
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2011-11-09 07:03:43 +0000

    slf4j

--- common/src/main/java/com/xiaoxiang/Main.java
+++ common/src/main/java/com/xiaoxiang/Main.java
@@ -1,26 +1,39 @@
 package com.xiaoxiang;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 测试类
- * 
+ *
  * @author xiang.xiaox
  */
 
 public class Main {
-	private List<String> list = new ArrayList<String>();
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private List<String> list = new ArrayList<String>();
 
-	@Override
-	public String toString() {
-		return "435345786肖祥7890890890890890890";
-	}
+    @Override
+    public String toString() {
+        return "435345786肖祥7890890890890890890";
+    }
 
-	public List<String> getList() {		
-		for (int i = 0; i < 10; i++) {
-			list.add("次夺" + i);
-		}
-		return list;
-	}
+    public List<String> getList() {
+        logger.debug("得到debug信息8");
+        logger.info("得到info信息8");
+        logger.warn("得到warn信息8");
+        logger.error("得到error信息8");
+        try{
+           int d = 1/0;
+        }catch (Exception e){
+            logger.error("出现异常",e);
+        }
+        for (int i = 0; i < 10; i++) {
+            list.add("次夺" + i);
+        }
+        return list;
+    }
 }
