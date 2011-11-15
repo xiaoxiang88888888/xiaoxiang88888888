6c3e77179bd499c882a76bf16c99292a53bcbb10
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2011-11-15 09:01:38 +0000

    maven  SLF4J

--- config-maven-plugin/src/main/java/com/xiaoxiang/maven/plugin/ConfigMojo.java
+++ config-maven-plugin/src/main/java/com/xiaoxiang/maven/plugin/ConfigMojo.java
@@ -3,6 +3,8 @@ package com.xiaoxiang.maven.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 说明
@@ -12,20 +14,21 @@ import org.apache.maven.plugin.MojoFailureException;
  */
 
 public class ConfigMojo extends AbstractMojo {
+    private final Logger logger =  LoggerFactory.getLogger(ConfigMojo.class);
 
     /**
      * 自定义的属性文件,用于动态生成其它文件的数据源
      *
      * @parameter expression="${propertiesPath}" default-value="${user.home}"
      */
-     String propertiesPath;
+    String propertiesPath;
 
     /**
      * 模块类型 freemarker 或者 velocity
      *
      * @parameter expression="${templateType}" default-value="freemarker"
      */
-     String templateType;
+    String templateType;
 
     /**
      * 文件编码
@@ -34,10 +37,19 @@ public class ConfigMojo extends AbstractMojo {
      */
     String encoding;
 
+    /**
+     * 工程名称
+     *
+     * @parameter expression="${project.build.finalName}"
+     */
+    String finalName;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        logger.info("//////////////finalName=" + finalName);
         getLog().info("//////////////propertiesPath=" + propertiesPath);
         getLog().info("//////////////templateType=" + templateType);
         getLog().info("//////////////encoding=" + encoding);
+        getLog().info("//////////////finalName=" + finalName);
     }
 }
