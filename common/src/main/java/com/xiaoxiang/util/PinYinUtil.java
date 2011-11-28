953cddfe9e01ea1e163e3137b000cac5b68537ac
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2011-11-28 12:00:03 +0000

    pinyin4j 加入

--- common/src/main/java/com/xiaoxiang/util/PinYinUtil.java
+++ common/src/main/java/com/xiaoxiang/util/PinYinUtil.java
@@ -67,4 +67,78 @@ public class PinYinUtil {
         return Collator.getInstance(Locale.CHINESE).compare(getPingYin(str1),
                 getPingYin(str2));
     }
+
+
+    /**
+     * 将汉字转换为全拼
+     *
+     * @param src
+     * @return String
+     */
+    public static String getPinYin(String src) {
+        char[] t1;
+        t1 = src.toCharArray();
+        String[] t2;
+        // 设置汉字拼音输出的格式
+        HanyuPinyinOutputFormat t3 = new HanyuPinyinOutputFormat();
+        t3.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        t3.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        t3.setVCharType(HanyuPinyinVCharType.WITH_V);
+        String t4 = "";
+        int t0 = t1.length;
+        try {
+            for (int i = 0; i < t0; i++) {
+                // 判断能否为汉字字符
+                if (Character.toString(t1[i]).matches("[\\u4E00-\\u9FA5]+")) {
+                    t2 = PinyinHelper.toHanyuPinyinStringArray(t1[i], t3);// 将汉字的几种全拼都存到t2数组中
+                    t4 += t2[0];// 取出该汉字全拼的第一种读音并连接到字符串t4后
+                } else {
+                    // 如果不是汉字字符，间接取出字符并连接到字符串t4后
+                    t4 += Character.toString(t1[i]);
+                }
+            }
+        } catch (BadHanyuPinyinOutputFormatCombination e) {
+            e.printStackTrace();
+        }
+        return t4;
+    }
+
+    /**
+     * 提取每个汉字的首字母
+     *
+     * @param str
+     * @return String
+     */
+    public static String getPinYinHeadChar(String str) {
+        String convert = "";
+        for (int j = 0; j < str.length(); j++) {
+            char word = str.charAt(j);
+            // 提取汉字的首字母
+            String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(word);
+            if (pinyinArray != null) {
+                convert += pinyinArray[0].charAt(0);
+            } else {
+                convert += word;
+            }
+        }
+        return convert;
+    }
+
+    /**
+     * 将字符串转换成ASCII码
+     *
+     * @param cnStr
+     * @return String
+     */
+    public static String getCnASCII(String cnStr) {
+        StringBuffer strBuf = new StringBuffer();
+        // 将字符串转换成字节序列
+        byte[] bGBK = cnStr.getBytes();
+        for (int i = 0; i < bGBK.length; i++) {
+            // 将每个字符转换成ASCII码
+            strBuf.append(Integer.toHexString(bGBK[i] & 0xff));
+        }
+        return strBuf.toString();
+    }
+
 }
