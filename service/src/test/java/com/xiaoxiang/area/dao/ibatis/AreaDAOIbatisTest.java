d3566d497088069132458e579625be038c40b6b0
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2011-11-30 13:27:26 +0000

    工具类单元测试  spring dao

--- service/src/test/java/com/xiaoxiang/area/dao/ibatis/AreaDAOIbatisTest.java
+++ service/src/test/java/com/xiaoxiang/area/dao/ibatis/AreaDAOIbatisTest.java
@@ -1,18 +1,30 @@
 package com.xiaoxiang.area.dao.ibatis;
 
+import com.xiaoxiang.area.dao.AreaDAO;
+import com.xiaoxiang.model.Area;
 import org.jtester.testng.JTester;
+import org.jtester.unitils.spring.SpringBeanFor;
 import org.testng.annotations.Test;
+import org.unitils.spring.annotation.SpringApplicationContext;
+import org.unitils.spring.annotation.SpringBeanByName;
+import org.unitils.spring.annotation.SpringBeanByType;
+
+import java.util.List;
 
 /**
  * 地区 dao测试
  *
  * @author xiang.xiaox
  */
-
+@SpringApplicationContext({"bean/spring-datasource.xml", "classpath:bean/spring-area.xml"})
 public class AreaDAOIbatisTest extends JTester {
 
-    @Test
-    public void isEMailStr(){
+    @SpringBeanByType
+    private AreaDAO areaDAO;
 
+    @Test
+    public void getAllEntityTest() {
+        List<Area> list = areaDAO.getAllEntity();
+        System.out.println(list.size());
     }
 }
