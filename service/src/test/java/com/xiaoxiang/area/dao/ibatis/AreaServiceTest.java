77ec2e83f3dbafdf183e3e1ffe93f1597a78af29
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2011-12-02 13:39:24 +0000

    测试

--- service/src/test/java/com/xiaoxiang/area/dao/ibatis/AreaServiceTest.java
+++ service/src/test/java/com/xiaoxiang/area/dao/ibatis/AreaServiceTest.java
@@ -1,11 +1,12 @@
 package com.xiaoxiang.area.dao.ibatis;
 
+
 import com.xiaoxiang.area.service.AreaService;
 import com.xiaoxiang.model.Area;
 import org.jtester.testng.JTester;
 import org.testng.annotations.Test;
 import org.unitils.spring.annotation.SpringApplicationContext;
-import org.unitils.spring.annotation.SpringBeanByName;
+import org.unitils.spring.annotation.SpringBeanByType;
 
 import java.util.List;
 
@@ -17,11 +18,21 @@ import java.util.List;
 @SpringApplicationContext({"bean/spring-datasource.xml", "classpath:bean/spring-area.xml"})
 public class AreaServiceTest extends JTester {
 
-    @SpringBeanByName
+    @SpringBeanByType
     private AreaService areaService;
+
+   /* @Mocked
+    @MockBean
+    private AreaDAO dao;*/
 
     @Test
     public void getAllEntityTest() {
+       /* new Expectations() {
+            {
+                dao.getAllEntity();
+                result = new ArrayList<Area>();
+            }
+        };*/
         List<Area> list = areaService.getAllEntity();
         System.out.println("servcie===" + list.size());
     }
