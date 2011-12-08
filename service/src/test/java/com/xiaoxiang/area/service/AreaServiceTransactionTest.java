912a84729b8c38f0e9ea01d7f8f2b8e412d40759
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2011-12-08 09:37:32 +0000

    事务管理测试完善

--- service/src/test/java/com/xiaoxiang/area/service/AreaServiceTransactionTest.java
+++ service/src/test/java/com/xiaoxiang/area/service/AreaServiceTransactionTest.java
@@ -3,6 +3,7 @@ package com.xiaoxiang.area.service;
 
 import com.xiaoxiang.model.Area;
 import com.xiaoxiang.util.StringUtil;
+import org.jtester.annotations.Transactional;
 import org.jtester.testng.JTester;
 import org.testng.annotations.Test;
 import org.unitils.spring.annotation.SpringApplicationContext;
@@ -19,20 +20,16 @@ public class AreaServiceTransactionTest extends JTester {
     private AreaService areaService;
 
     @Test
+    @Transactional(Transactional.TransactionMode.COMMIT)
     public void insertAndDeleteAreaTest() {
         String id = StringUtil.getUUID();
-        Area area = new Area();
-        area.setId(id);
-        area.setAreaId(id);
-        area.setAreacode("id");
-        area.setAreaname("id");
-        area.setOrderno(0);
-        area.setParentAreaId("0");
-        area.setRemark("测试事务");
-
+        System.out.println(id);
         boolean result = areaService.insertAndDeleteArea(id);
         System.out.println("result===" + result);
         System.out.println(areaService.exists("8"));
+        Area area = areaService.findEntityBykey(id);
+        System.out.println(area.getAreaId());
+        want.object(area).notNull();
     }
 
 
