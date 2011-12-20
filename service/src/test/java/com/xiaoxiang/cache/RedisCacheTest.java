f051113319cdf16c14c80d08317dda4aa9843261
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2011-12-20 07:50:05 +0000

    redis的代码添加,以及说明文档添加

---
+++ service/src/test/java/com/xiaoxiang/cache/RedisCacheTest.java
@@ -0,0 +1,69 @@
+package com.xiaoxiang.cache;
+
+import com.xiaoxiang.util.JsonUtil;
+import org.jtester.testng.JTester;
+import org.testng.annotations.Test;
+import org.unitils.spring.annotation.SpringApplicationContext;
+import org.unitils.spring.annotation.SpringBeanByType;
+
+/**
+ * Redis 缓存测试
+ *
+ * @author xiang.xiaox
+ */
+@SpringApplicationContext({"bean/spring-cache.xml"})
+public class RedisCacheTest extends JTester {
+    @SpringBeanByType
+    private CacheSupport cacheSupport;
+
+    @Test
+    public void setTest() {
+        String key = "0000";
+        String value = "9999";
+        cacheSupport.cacheTestSet(key, value);
+        String result = cacheSupport.cacheTestGet(key);
+        result = JsonUtil.getInstance().jsonToObject(result,String.class).toString();
+        want.string("9999").isEqualTo(result);
+    }
+
+    @Test
+    public void setExpireTest() {
+        String key = "0000";
+        String value = "9999";
+        int expire = 10;
+        cacheSupport.cacheTestSet(key, value, expire);
+        String result = cacheSupport.cacheTestGet(key);
+        result = JsonUtil.getInstance().jsonToObject(result,String.class).toString();
+        want.string("9999").isEqualTo(result);
+        try {
+            Thread.sleep(10000);//括号里面的10000代表10000毫秒，也就是10秒，可以该成你需要的时间
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        want.object(cacheSupport.cacheTestGet(key)).isNull();
+    }
+
+    @Test
+    public void getTest() {
+        String key = "0000";
+        String value = "9999";
+        cacheSupport.cacheTestSet(key, value);
+        String result = cacheSupport.cacheTestGet(key);
+        result = JsonUtil.getInstance().jsonToObject(result,String.class).toString();
+        want.string("9999").isEqualTo(result);
+    }
+
+    @Test
+    public void delTest() {
+        String key = "0000";
+        String value = "9999";
+        cacheSupport.cacheTestSet(key, value);
+        String result = cacheSupport.cacheTestGet(key);
+        result = JsonUtil.getInstance().jsonToObject(result,String.class).toString();
+        want.string("9999").isEqualTo(result);
+        cacheSupport.cacheTestDel(key);
+        want.object(cacheSupport.cacheTestGet(key)).isNull();
+    }
+
+
+}
