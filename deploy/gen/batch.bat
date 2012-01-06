d2addf417637bde8d2a918334b32cf9f13f89d68
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2012-01-06 10:18:24 +0000

    代码生成脚本完善,增加批量操作 使用call,不中止当前进程

--- deploy/gen/batch.bat
+++ deploy/gen/batch.bat
@@ -1,8 +1,8 @@
 @echo off
 
 @rem 批量生成
-ant genTable -DtableName=area  -DtemplateDir=vo  -DtemplateName=VO.vm -DfileName=Area.java
-ant genTable -DtableName=usergroup  -DtemplateDir=vo  -DtemplateName=VO.vm -DfileName=UserGroup.java
-ant genTable -DtableName=role  -DtemplateDir=vo  -DtemplateName=VO.vm -DfileName=Role.java
+call ant genTable -DtableName=area  -DtemplateDir=vo  -DtemplateName=VO.vm -DfileName=Area.java
+call ant genTable -DtableName=usergroup  -DtemplateDir=vo  -DtemplateName=VO.vm -DfileName=UserGroup.java
+call ant genTable -DtableName=role  -DtemplateDir=vo  -DtemplateName=VO.vm -DfileName=Role.java
 
 
