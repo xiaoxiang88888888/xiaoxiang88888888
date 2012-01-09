7aa0f8a1023a009f8a22f9a32163208839ff647e
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2012-01-09 09:39:32 +0000

    配置

--- ticket/src/main/java/com/xiaoxiang/ticket/Main.java
+++ ticket/src/main/java/com/xiaoxiang/ticket/Main.java
@@ -1,5 +1,9 @@
 package com.xiaoxiang.ticket;
 
+import com.xiaoxiang.ticket.util.ConstantUtil;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * 说明
  *
@@ -8,6 +12,9 @@ package com.xiaoxiang.ticket;
 
 public class Main {
     public void getApplication() {
-
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean/spring-bean.xml");
+        ConstantUtil constantUtil = (ConstantUtil)context.getBean("constant");
+        System.out.println(constantUtil);
     }
 }
---
+++ ticket/src/test/java/com/xiaoxiang/ticket/MainTest.java
@@ -0,0 +1,17 @@
+package com.xiaoxiang.ticket;
+
+import junit.framework.TestCase;
+
+/**
+ * 说明
+ *
+ * @author xiang.xiaox
+ */
+
+public class MainTest extends TestCase {
+    public void testTicket() {
+        Main main = new Main();
+        main.getApplication();
+    }
+
+}
