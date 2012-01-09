47beb301a19f49995140f5a9f58f2aa898969241
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2012-01-09 09:32:56 +0000

    一个小工具

---
+++ ticket/src/main/java/com/xiaoxiang/ticket/Main.java
@@ -0,0 +1,13 @@
+package com.xiaoxiang.ticket;
+
+/**
+ * 说明
+ *
+ * @author xiang.xiaox
+ */
+
+public class Main {
+    public void getApplication() {
+
+    }
+}
---
+++ ticket/src/main/java/com/xiaoxiang/ticket/util/ConstantUtil.java
@@ -0,0 +1,30 @@
+package com.xiaoxiang.ticket.util;
+
+import java.util.Properties;
+
+/**
+ * 常量导入
+ *
+ * @author xiang.xiaox
+ */
+
+public class ConstantUtil {
+    private Properties seatProp;
+    private Properties stationProp;
+
+    public Properties getSeatProp() {
+        return seatProp;
+    }
+
+    public void setSeatProp(Properties seatProp) {
+        this.seatProp = seatProp;
+    }
+
+    public Properties getStationProp() {
+        return stationProp;
+    }
+
+    public void setStationProp(Properties stationProp) {
+        this.stationProp = stationProp;
+    }
+}
