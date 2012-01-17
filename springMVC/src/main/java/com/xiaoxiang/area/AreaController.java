9a7acf93060469994e21537deb628456a4252d40
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2012-01-17 08:33:45 +0000

    一些小修改

--- springMVC/src/main/java/com/xiaoxiang/area/AreaController.java
+++ springMVC/src/main/java/com/xiaoxiang/area/AreaController.java
@@ -3,7 +3,10 @@ package com.xiaoxiang.area;
 import com.xiaoxiang.area.service.AreaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -19,8 +22,8 @@ public class AreaController {
     @RequestMapping(value = "/jsp")
     public ModelAndView helloJsp() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("template/area/area_list.jsp");
-        modelAndView.addObject("message", "Hello,JSP");
+        modelAndView.setViewName("/area/areaList.vm");
+        modelAndView.addObject("message", "Hello,JSP,中文测试");
         return modelAndView;
     }
 
@@ -31,8 +34,14 @@ public class AreaController {
         ModelAndView modelAndView = new ModelAndView();
         //modelAndView.setView(new VelocityView());
         modelAndView.setViewName("/area/areaList.vm");
-        modelAndView.addObject("message", "Hello,Velocity");
+        modelAndView.addObject("message", "Hello,Velocity,中文测试");
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/login/{user}", method = RequestMethod.GET)
+    public ModelAndView myMethod(@PathVariable("user") String user, ModelMap modelMap) throws Exception {
+        modelMap.put("loginUser", user);
+        return new ModelAndView("/area/areaList.vm", modelMap);
     }
 
 }
