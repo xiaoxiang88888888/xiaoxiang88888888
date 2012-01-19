cb0f7b973f254da0bdf85fbdb4fb65c82d9142f4
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2012-01-19 05:33:16 +0000

    springmvc的一些学习,页面的代码调试

--- static/js/app/area/index.js
+++ static/js/app/area/index.js
@@ -1,22 +1,26 @@
 //地区主页面
 var areaIndex = function($) {
+    var jsonData;
     //全局
     var time = 5000;//5秒钟后消失 
     return {
         //初始化相关数据
         init:function() {
-
+           areaIndex.loadData();
+           areaIndex.bind();
         },
-        loadData:function(callback){
+        //载入数据
+        loadData:function(callbacks){
             $.ajax({
                 'type':'get',
                 'cache':false,
-                'url':(webServer + '/jcrmgroup!groups.jspa'),
+                'url':(webServer + '/area/list.json'),
                 success:function(data){
-                    if(data && data.groupList){
-                        window.__customer_group = data.groupList;
+                    if(data && data.result){
+                        areaIndex.renderData(data.data);
                     }else{
-                        window.location.href="/exception/exception.vm";
+                        alert("出现错误:"+data.errorDesc);
+                        return;
                     }
                     //回调
                     if(callbacks) {
@@ -26,11 +30,70 @@ var areaIndex = function($) {
                     }
                 }
             });
+        },
+        //页面渲染
+        renderData:function(data,areaname){
+            var html = '<ui>';
+            var list = data;
+            for (var i = 0; i < list.length; i++) {
+                var item = list[i];
+                html += '<li>' + item.areaname + '<button name="updateArea" data="'+item.areaId+'">修改</button><button name="deleteArea" data="'+item.areaId+'">删除</button></li>';
+            }
+            html += '</ui>';
+           $('#data').html(html);
+        },
+        //绑定相关事件
+        bind:function(){
+            //新增地区
+            $('#add').click(function() {
+                window.location.href='/area/addArea.htm';
+            });
+            //修改
+            $('#data').delegate('button[name="updateArea"]', 'click', function() {
+                areaIndex.updateArea($(this).attr('data'));
+                return false;
+            });
+             //删除
+            $('#data').delegate('button[name="deleteArea"]', 'click', function() {
+                areaIndex.deleteArea($(this).attr('data'));
+                return false;
+            });
+        },
+        updateArea:function(id){
+
+        },
+        deleteArea:function (id) {
+            var data = {
+                'areaId':id
+            };
+            if (confirm('您确定要删除该地区吗？')) {
+                $.ajax({
+                    type:'delete',
+                    data:data,
+                    url:'/area/'+id+".json",
+                    cache:false,
+                    success:function (message) {
+                        if (message&&message.result) {
+                            alert("删除成功");
+                        } else {
+                            alert("删除失败");
+                        }
+                        //刷新数据
+                        window.location.href='/area/index.htm';
+                    }
+                });
+            }
         }
-	};
+    };
 }(jQuery);
-//调用初始
-areaIndex.init();
+
+//加载
+$('document').ready(function() {
+    //调用初始
+    areaIndex.init();
+});
+
+
 
 
 
