cfa3614e537a65ccb38fe9cbe153e4644c41bfcf
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2012-01-20 02:26:31 +0000

    springmvc的一些学习,多文件上传

--- springMVC/src/main/java/com/xiaoxiang/fileupload/FileUploadController.java
+++ springMVC/src/main/java/com/xiaoxiang/fileupload/FileUploadController.java
@@ -1,12 +1,16 @@
 package com.xiaoxiang.fileupload;
 
 import com.xiaoxiang.result.JSONResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,20 +18,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/fileupload")
 public class FileUploadController {
+    protected static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
+
+
     private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
-    private String configPath = null;
-    private String uploadPath = File.separator + "fileupload" + File.separator;
-    private String uploadPathTemp = File.separator + "fileupload" + File.separator;
-    private String tempPath = "";
 
     /**
      * 初始化相关设置 设置上传路径
      */
-    public void init(HttpServletRequest request) {
+    public String init(HttpServletRequest request) {
+        String configPath = null;
+        String uploadPath = File.separator + "fileupload" + File.separator;
+        String uploadPathTemp = File.separator + "fileupload" + File.separator;
+        String tempPath = "";
         configPath = request.getSession().getServletContext().getRealPath("") + System.getProperty("file.separator");
         uploadPath = configPath + uploadPath;
         tempPath = uploadPath + "tmp" + File.separator;
@@ -36,7 +44,7 @@ public class FileUploadController {
         if (!fileUpload.exists()) {
             if (!fileUpload.mkdir()) {
                 System.out.println("无法创建存储目录!");
-                return;
+                return null;
             }
         }
         if (!fileUploadTemp.exists()) {
@@ -44,6 +52,7 @@ public class FileUploadController {
                 System.out.println("无法创建临时存储目录!");
             }
         }
+        return uploadPath;
     }
 
     @RequestMapping(value = "/index.htm", method = RequestMethod.GET)
@@ -53,50 +62,34 @@ public class FileUploadController {
         return modelAndView;
     }
 
-    @ModelAttribute
-    public void ajaxAttribute(WebRequest request, Model model) {
-        model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(request));
-    }
-
     @RequestMapping(method = RequestMethod.GET)
     public void fileUploadForm() {
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
-    public JSONResult processUpload(@RequestParam MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public JSONResult processUpload(@RequestParam String param,MultipartHttpServletRequest request, HttpServletResponse response) throws IOException {
         JSONResult result = new JSONResult();
         try {
-            init(request);
+            System.out.println(param);
+            String uploadPath = init(request);
             response.setContentType(CONTENT_TYPE);           
             request.setCharacterEncoding("UTF-8");
-            CommonsMultipartFile files = (CommonsMultipartFile)file;
-            File fileTemp = new File(uploadPath +file.getName());
-            FileCopyUtils.copy(file.getBytes(),fileTemp);
+            //取所有name等于file,且type='file'的元素
+            List<MultipartFile> files = request.getFiles("file");
+            for(MultipartFile file:files){
+                File fileTemp = new File(uploadPath +((CommonsMultipartFile) file).getFileItem().getName());
+                FileCopyUtils.copy(file.getBytes(),fileTemp);
+                logger.info("File '" + file.getOriginalFilename() + "' uploaded successfully");
+            }
             //
             result.setResult(true);
-            result.setData("File '" + file.getOriginalFilename() + "' uploaded successfully");
+            result.setData(null);
         } catch (Exception e) {
             result.setResult(false);
             result.setErrorDesc("出现异常:" + e.getMessage());
         }
         return result;
-    }
-
-}
-
-class AjaxUtils {
-
-    public static boolean isAjaxRequest(WebRequest webRequest) {
-        String requestedWith = webRequest.getHeader("X-Requested-With");
-        return requestedWith != null ? "XMLHttpRequest".equals(requestedWith) : false;
-    }
-
-    public static boolean isAjaxUploadRequest(WebRequest webRequest) {
-        return webRequest.getParameter("ajaxUpload") != null;
-    }
-
-    private AjaxUtils() {
     }
 
 }
