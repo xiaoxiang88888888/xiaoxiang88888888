892085e1f9e0cf19911c2f494cb34d953df8c026
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2012-02-13 11:59:22 +0000

    hudson findbugs

--- concurrent/src/main/java/com/xiaoxiang/concurrent/Main.java
+++ concurrent/src/main/java/com/xiaoxiang/concurrent/Main.java
@@ -6,11 +6,32 @@ package com.xiaoxiang.concurrent;
  * @author xiang.xiaox
  */
 public class Main {
+    final static UnSafeSeq seq = new UnSafeSeq();
 
     public static void main(String[] args) {
-        UnSafeSeq seq = new UnSafeSeq();
-        System.out.println(seq.getNextNum());
-        Thread t = new Thread();
+        seq.getThreadLocal().set("88");
+        int threadNum = 60;
+        Thread[] t = new Thread[threadNum];
+        for (int i = 0; i < threadNum; i++) {
+            t[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0; j < 1000; j++) {
+                        seq.getNextaLong();
+                        //seq.getNextNumSafe();
+                        //seq.getNextNum();
+                        System.out.println("线程 NextNum=" + seq.getaLong());
+
+                    }
+                }
+            });
+            t[i].start();
+        }
+       /* while (Thread.activeCount() >= 1) {
+            Thread.yield();
+        }
+
+        System.out.println("合计="+seq.getNum());*/
 
     }
 }
--- concurrent/src/main/java/com/xiaoxiang/concurrent/UnSafeSeq.java
+++ concurrent/src/main/java/com/xiaoxiang/concurrent/UnSafeSeq.java
@@ -1,5 +1,9 @@
 package com.xiaoxiang.concurrent;
 
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  * 不安全的序列获取
  * num++ 分三步执行 读取 +1 写入
@@ -8,13 +12,43 @@ package com.xiaoxiang.concurrent;
  */
 
 public class UnSafeSeq {
+    private ThreadLocal threadLocal = new ThreadLocal();
+
+    private AtomicLong aLong = new AtomicLong();
     private int num;
+
+    private AtomicReference<BigDecimal> bigDecimalAtomicReference= new AtomicReference<BigDecimal>();
 
     public int getNextNum() {
         return num++;
     }
 
+    public int getNum() {
+        return num;
+    }
+
+    public ThreadLocal getThreadLocal() {
+        return threadLocal;
+    }
+
     synchronized public int getNextNumSafe() {
         return num++;
+    }
+
+    public long getaLong() {
+        return aLong.get();
+    }
+
+    public long getNextaLong() {
+        return aLong.incrementAndGet();
+    }
+
+    public AtomicReference<BigDecimal> getBigDecimalAtomicReference() {
+        bigDecimalAtomicReference.get();
+        return bigDecimalAtomicReference;
+    }
+
+    public AtomicReference<BigDecimal> getNextBigDecimalAtomicReference() {
+        return bigDecimalAtomicReference;
     }
 }
