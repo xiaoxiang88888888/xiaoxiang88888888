ae29978d65c28b798b5ecfab8a88042fc953b93b
xiaoxiang88888888 <xiaoxiang88888888@60e2a55e-d7d9-11dd-90d0-c9c6575c7768>
2012-02-20 08:38:10 +0000

    冒泡排序,支持desc ,asc,以及泛型

--- tool/src/main/java/com/xiaoxiang/tool/ds/sort/BubbleSort.java
+++ tool/src/main/java/com/xiaoxiang/tool/ds/sort/BubbleSort.java
@@ -7,41 +7,48 @@ package com.xiaoxiang.tool.ds.sort;
  * @author xiang.xiaox
  */
 
-public class BubbleSort {  
-      
-    public void swap(int[] array ,int one,int two){  
-        int temp = array[one];  
-        array[one]=array[two];  
-        array[two]=temp;  
-    }  
-  
-    //1、冒泡排序  
-    public int[] bubbleSort(int[] array){  
-        //不是正在意义的冒泡排序,更像选择排序  
-        for (int i = 0; i < array.length; i++) {  
-            for (int j = 0; j < array.length; j++) {  
-                if(array[i]<array[j]){  
-                    swap(array,i,j);  
-                }  
-            }  
-        }  
-        /* 
-         * 正在意义的冒泡排序，效率也比上面的要高 
-         * 以数组长度10个为例，第一次需要比较9次，第二次8次，以此类推， 
-         * 每次比较之后，数组最右边的数据时有序的了。 
-         * 所以时间复杂度为9+8+7。。。+1=N*(N-1)/2 
-         * 算法作约，复杂度为N*N/2，几位O(N*N); 
-         */  
-        //注意外层循环的out>1,可以减少一次多余的循环。  
-        for(int out = array.length-1;out>1;out--){  
-            for (int in = 0; in < out; in++) {  
-                if(array[in]>array[in+1]){  
-                    swap(array,in,in+1);  
-                }  
-            }  
-        }  
-        return array;  
+public class BubbleSort {
+
+    public static <T extends Comparable> void swap(T[] array, int one, int two) {
+        T temp = array[one];
+        array[one] = array[two];
+        array[two] = temp;
     }
-      
+
+
+    /**
+     * 真正意义的冒泡排序，效率也比较高
+     * 以数组长度10个为例，第一次需要比较9次，第二次8次，以此类推，
+     * 每次比较之后，数组最右边的数据时有序的了。
+     * 所以时间复杂度为9+8+7。。。+1=N*(N-1)/2
+     * 算法作约，复杂度为N*N/2，几位O(N*N);
+     * 注意外层循环的out>1,可以减少一次多余的循环。
+     *
+     * @param array
+     * @param order desc从大到小 asc从小到大
+     * @param <T>
+     * @return
+     */
+    public static <T extends Comparable> T[] sort(T[] array, String order) {
+        if (order == null || !"desc".equalsIgnoreCase(order)) { //升序
+            for (int out = array.length - 1; out > 1; out--) {
+                for (int in = 0; in < out; in++) {
+                    if (array[in].compareTo(array[in + 1]) > 0) {
+                        swap(array, in, in + 1);
+                    }
+                }
+            }
+        } else {//降序
+            for(int out=0;out<array.length-1;out++){
+              for(int in=array.length-1;in>out;in--){
+                  if (array[in].compareTo(array[in - 1]) > 0) {
+                      swap(array, in, in - 1);
+                  }
+              }
+            }
+        }
+        return array;
+    }
+
 
 } 
--- tool/src/test/java/com/xiaoxiang/tool/ds/sort/BubbleSortTest.java
+++ tool/src/test/java/com/xiaoxiang/tool/ds/sort/BubbleSortTest.java
@@ -9,18 +9,88 @@ import junit.framework.TestCase;
  */
 
 public class BubbleSortTest  extends TestCase {
+
     /**
      * 测试
      */
-    public void testDesc(){
-        int[] array = new int[20];
+    public void testIntegerArrayDesc(){
+        Integer[] array = new Integer[20];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int)(Math.random()*45);
         }
-        BubbleSort sort = new BubbleSort();
-        sort.bubbleSort(array);
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        System.out.print("before:");
+        for (Integer anArray : array) {
+            System.out.print(anArray+",");
         }
+        System.out.println();
+        System.out.print("sort  :");
+        BubbleSort.sort(array,"desc");
+        for (Integer anArray : array) {
+            System.out.print(anArray+",");
+        }
+        System.out.println();
+    }
+
+    /**
+     * 测试
+     */
+    public void testIntegerArrayAsc(){
+        Integer[] array = new Integer[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()*45);
+        }
+        System.out.print("before:");
+        for (Integer anArray : array) {
+            System.out.print(anArray+",");
+        }
+        System.out.println();
+        System.out.print("sort  :");
+        BubbleSort.sort(array,"asc");
+        for (Integer anArray : array) {
+            System.out.print(anArray+",");
+        }
+        System.out.println();
+    }
+
+    /**
+     * 测试
+     */
+    public void testDoubleArrayDesc(){
+        Double[] array = new Double[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random()*45;
+        }
+        System.out.print("before:");
+        for (Double anArray : array) {
+            System.out.print(anArray + ",");
+        }
+        System.out.println();
+        System.out.print("sort  :");
+        BubbleSort.sort(array,"desc");
+        for (Double anArray : array) {
+            System.out.print(anArray + ",");
+        }
+        System.out.println();
+    }
+
+    /**
+     * 测试
+     */
+    public void testDoubleArrayAsc(){
+        Double[] array = new Double[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random()*45;
+        }
+        System.out.print("before:");
+        for (Double anArray : array) {
+            System.out.print(anArray + ",");
+        }
+        System.out.println();
+        System.out.print("sort  :");
+        BubbleSort.sort(array,"asc");
+        for (Double anArray : array) {
+            System.out.print(anArray + ",");
+        }
+        System.out.println();
     }
 }
